2d03a8bf7881530566070c48d897a676509bf8b7
bk148 <98980843+bk148@users.noreply.github.com>
2023-08-29 22:13:16 +0200

    update test for lettings and profiles, and create bakup.json

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    assert 1
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestIndex(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('home'))
+        assert response.status_code == 200
+
+    def test_title(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, "Welcome to Holiday Homes")
+
+
+
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 
-app_name = 'c_lettings_site'
+app_name = 'oc_lettings_site'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
